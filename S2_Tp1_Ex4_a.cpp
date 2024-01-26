754c3420e578c54fd6cc910f803f43ad882cfb6b
NadiaFrikha12 <148723455+NadiaFrikha12@users.noreply.github.com>
2024-01-26 17:37:13 +0100

    Update S2_Tp1_Ex4_a.cpp

--- S2_Tp1_Ex4_a.cpp
+++ S2_Tp1_Ex4_a.cpp
@@ -1,6 +1,6 @@
-//Ecrire un programme qui élimine les répétitions d’espaces dans un fichier texte.
-//Dans des traitements indépendants les uns des autres, le résultat sera :
-//a- Affiché sur l’écran
+//Ecrire un programme qui Ã©limine les rÃ©pÃ©titions dâ€™espaces dans un fichier texte.
+//Dans des traitements indÃ©pendants les uns des autres, le rÃ©sultat sera :
+//a- AffichÃ© sur lâ€™Ã©cran
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -9,10 +9,9 @@
 
 int main(){
 	
-	//verifier si le pointeur du fichier est valide
 	FILE *fp;
-	char nom[10]; //le nom du fichier à ouvrir
-	char ligne[100]; //pour désigner chaque ligne du fichier
+	char nom[10]; //le nom du fichier Ã  ouvrir
+	char ligne[100]; //pour dÃ©signer chaque ligne du fichier
 	int i=0, j=0; //compteurs
 	
 	//lire le nom du fichier
@@ -22,6 +21,7 @@ int main(){
 	//ouvrir le fichier en mode read
 	fp=fopen(nom,"r");
 	
+	//verifier si le pointeur du fichier est valide
 	if (fp == NULL){
 		printf("le fichier n'a pas put etre ouvert");
 		return 0;
@@ -35,7 +35,7 @@ int main(){
 				j++;	
 			}	
 		}
-		ligne[j] = '\0'; // Ajouter le caractère de fin de chaîne à la nouvelle ligne
+		ligne[j] = '\0'; // Ajouter le caractÃ¨re de fin de chaÃ®ne Ã  la nouvelle ligne
 		
 		// Afficher la ligne sur l'ecran sans exces d'espace
         printf("%s", ligne);
